5d9248b278aa2f1fbf4df76e69a90712f9938529
Alexandre Marcireau <alexandre.marcireau@gmail.com>
2021-05-03 10:11:48 +0200

    Add a filter for command-line apps

--- premake4.lua
+++ premake4.lua
@@ -5,42 +5,50 @@ newoption {
     description = "Disable GUI apps and enable the external interface"
 }
 
+newoption {
+    trigger = "command-line",
+    description = "Compile only command line tools"
+}
+
 solution 'atis'
     configurations {'release', 'debug'}
     location 'build'
 
-    project 'recorder'
-        kind 'ConsoleApp'
-        language 'C++'
-        location 'build'
-        files {'source/parse_parameters.hpp', 'source/interface.hpp', 'source/recorder.cpp'}
-        defines {'SEPIA_COMPILER_WORKING_DIRECTORY="' .. project().location .. '"'}
-        if _OPTIONS['raspberry-pi'] then
-            defines {'RASPBERRY_PI'}
-        end
-        configuration 'release'
-            targetdir 'build/release'
-            defines {'NDEBUG'}
-            flags {'OptimizeSpeed'}
-        configuration 'debug'
-            targetdir 'build/debug'
-            defines {'DEBUG'}
-            flags {'Symbols'}
-        configuration 'linux'
-            links {'pthread', 'usb-1.0', 'stdc++fs'}
-            buildoptions {'-std=c++17', '-Wno-psabi'}
-            linkoptions {'-std=c++17', '-Wno-psabi'}
-        configuration 'macosx'
-            includedirs {'/usr/local/include'}
-            libdirs {'/usr/local/lib'}
-            links {'usb-1.0'}
-            buildoptions {'-std=c++17'}
-            linkoptions {'-std=c++17'}
-        configuration 'windows'
-            files {'.clang-format'}
-            includedirs {'C:\\Include'}
-            links {'C:\\Windows\\SysWOW64\\libusb-1.0'}
-            buildoptions {'/std:c++17'}
+    if not _OPTIONS['command-line'] then
+        project 'recorder'
+            kind 'ConsoleApp'
+            language 'C++'
+            location 'build'
+            files {'source/parse_parameters.hpp', 'source/interface.hpp', 'source/recorder.cpp'}
+            defines {'SEPIA_COMPILER_WORKING_DIRECTORY="' .. project().location .. '"'}
+            if _OPTIONS['raspberry-pi'] then
+                defines {'RASPBERRY_PI'}
+            end
+            configuration 'release'
+                targetdir 'build/release'
+                defines {'NDEBUG'}
+                flags {'OptimizeSpeed'}
+            configuration 'debug'
+                targetdir 'build/debug'
+                defines {'DEBUG'}
+                flags {'Symbols'}
+            configuration 'linux'
+                links {'pthread', 'usb-1.0', 'stdc++fs'}
+                buildoptions {'-std=c++17', '-Wno-psabi'}
+                linkoptions {'-std=c++17', '-Wno-psabi'}
+            configuration 'macosx'
+                includedirs {'/usr/local/include'}
+                libdirs {'/usr/local/lib'}
+                links {'usb-1.0'}
+                buildoptions {'-std=c++17'}
+                linkoptions {'-std=c++17'}
+            configuration 'windows'
+                files {'.clang-format'}
+                includedirs {'C:\\Include'}
+                links {'C:\\Windows\\SysWOW64\\libusb-1.0'}
+                buildoptions {'/std:c++17'}
+    end
+
 
     if not _OPTIONS['raspberry-pi'] then
         project 'raw_to_es'
@@ -67,7 +75,9 @@ solution 'atis'
             configuration 'windows'
                 files {'.clang-format'}
                 buildoptions {'/std:c++17'}
+    end
 
+    if not _OPTIONS['raspberry-pi'] and not _OPTIONS['command-line'] then
         project 'viewer'
             kind 'ConsoleApp'
             language 'C++'
